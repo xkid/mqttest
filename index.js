9a6af1065337a00e5af067d11b14b5e197a157e4
xkid <chchin83@gmail.com>
2017-11-10 16:05:35 +0800

    Add Icon

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var index = fs.readFileSync('index.html');
 
 var http = require('http'),
     httpServ = http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(index);
+        if ((req.url == '/index.html') || (req.url == '/')){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(index);
+        }
+        if (req.url == '/mqtt.png'){
+            var icon = fs.readFileSync('./mqtt.png');
+            res.writeHead(200, {'Content-Type': 'image/png' });
+            res.end(icon, 'binary');
+        }
+        res.end("No Found.");
+        
     }),
     mosca = require('mosca'),
     mqttServ = new mosca.Server({});
